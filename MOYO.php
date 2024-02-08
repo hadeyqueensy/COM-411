89c33fa5a045c27e21aa6a4df936ff7bc9db4380
hadeyqueensy <159326680+hadeyqueensy@users.noreply.github.com>
2024-02-08 05:37:15 -0800

    Add files via upload

---
+++ MOYO.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $aa = 8;
+
+        $i = 0;
+
+        $da = 3.99;
+
+        
+        $d = 0.0;
+
+        $str = "MOYO MACURLY";
+
+        echo $aa + $i . "<br>";
+
+        echo $da + $d ."<br>";
+
+        echo "Your name is ". $str;
+
+
+
+
+
+    ?>
+</body>
+</html>
